2cdf70f7deaa924318cbd9cd2785ae4fb30b5708
wnatvig <william@natvig.se>
2024-05-21 14:37:56 +0200

    Add files via upload

--- MA4/person.py
+++ MA4/person.py
@@ -1,14 +1,19 @@
 """ Python interface to the C++ Person class """
 import ctypes
-import os
+lib = ctypes.cdll.LoadLibrary('./libperson.so')
 
-# Determine the absolute path to the shared library
-current_dir = os.path.dirname(__file__)
-lib_path = os.path.join(current_dir, 'libperson.so')
-lib = ctypes.cdll.LoadLibrary(lib_path)
-
-class Person:
+class Person(object):
     def __init__(self, age):
+        lib.Person_new.argtypes = [ctypes.c_int]
+        lib.Person_new.restype = ctypes.c_void_p
+        lib.Person_getAge.argtypes = [ctypes.c_void_p]
+        lib.Person_getAge.restype = ctypes.c_int
+        lib.Person_setAge.argtypes = [ctypes.c_void_p, ctypes.c_int]
+        lib.Person_getDecades.argtypes = [ctypes.c_void_p]
+        lib.Person_getDecades.restype = ctypes.c_double
+        lib.Person_fib.argtypes = [ctypes.c_void_p]
+        lib.Person_fib.restype = ctypes.c_int
+        lib.Person_delete.argtypes = [ctypes.c_void_p]
         self.obj = lib.Person_new(age)
 
     def getAge(self):
@@ -21,29 +26,7 @@ class Person:
         return lib.Person_getDecades(self.obj)
 
     def fib(self):
-        if hasattr(lib, 'Person_fib'):
-            return lib.Person_fib(self.obj)
-        else:
-            raise NotImplementedError("The function Person_fib is not available in the shared library")
+        return lib.Person_fib(self.obj)
 
     def __del__(self):
-        if hasattr(self, 'obj'):
-            lib.Person_delete(self.obj)
-
-# Specify the argument and return types for the C functions
-lib.Person_new.argtypes = [ctypes.c_int]
-lib.Person_new.restype = ctypes.c_void_p
-lib.Person_getAge.argtypes = [ctypes.c_void_p]
-lib.Person_getAge.restype = ctypes.c_int
-lib.Person_setAge.argtypes = [ctypes.c_void_p, ctypes.c_int]
-lib.Person_setAge.restype = None
-lib.Person_getDecades.argtypes = [ctypes.c_void_p]
-lib.Person_getDecades.restype = ctypes.c_int
-
-# Only set the argument and return types for Person_fib if it exists
-if hasattr(lib, 'Person_fib'):
-    lib.Person_fib.argtypes = [ctypes.c_void_p]
-    lib.Person_fib.restype = ctypes.c_int
-
-lib.Person_delete.argtypes = [ctypes.c_void_p]
-lib.Person_delete.restype = None
+        return lib.Person_delete(self.obj)
