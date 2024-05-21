a8a55179be1dce5e25f013e630ef12aa6cf86afb
wnatvig <william@natvig.se>
2024-05-21 16:47:25 +0200

    Add files via upload

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -3,7 +3,7 @@
 from person import Person
 from fibonacci import fib_py, fib_numba
 import time
-
+import matplotlib.pyplot as plt
 
 def main():
 	f = Person(50)
@@ -16,6 +16,8 @@ def main():
 
 def main():
   
+    
+    
     p = Person(10)
     print("C++ Fibonacci:", p.fib())
 
@@ -28,6 +30,33 @@ def main():
     start = time.time()
     print("Numba Fibonacci:", fib_numba(10))
     print("Numba Time:", time.time() - start)
+    
+    x = range(20,30)
+    y_py = []
+    y_numba = []
+    y_cpp = []
+    
+    for i in range(20, 30):
+        start = time.time()
+        Person(i)
+        p.fib()
+        y_cpp.append(time.time() - start)
+
+    for i in range(20,30):
+        start = time.time()
+        fib_py(i)
+        y_py.append(time.time() - start)
+        
+
+    for i in range (20,30):
+        start=time.time()
+        fib_numba(i)
+        y_numba.append(time.time() - start)
+
+    plt.plot(x, y_py)
+    plt.plot(x, y_numba)
+    plt.show()
+    print (y_py, y_numba)
     
     
 if __name__ == '__main__':
