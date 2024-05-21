f396b0a3b5aeea676e0bc4b0fb51f1d174d79a4f
wnatvig <william@natvig.se>
2024-05-21 15:23:49 +0200

    Add files via upload

--- MA4/person.cpp
+++ MA4/person.cpp
@@ -7,7 +7,7 @@ class Person{
 		int getAge();
 		void setAge(int);
 		double getDecades();
-		int fib(int);
+		int fib();
 	private:
 		int age;
 	};
@@ -28,16 +28,16 @@ double Person::getDecades(){
 	return age/10;
 	}
 
-int Person::fib(int n){
-    if (n <= 1) return n;
-    return fib(n - 1) + fib(n - 2);
+int Person::fib(int a){
+    if (a <= 1) return a;
+    return fib(a - 1) + fib(a - 2);
     }
 
 extern "C"{
 	Person* Person_new(int a) {return new Person(a);}
 	int Person_getAge(Person* person) {return person->getAge();}
 	void Person_setAge(Person* person, int a) {person->setAge(a);}
-	int Person_fib(Person* person, int n) {return person->fib(n);}
+	int Person_fib(Person* person) {return person->fib();}
 	int Person_getDecades(Person* person) {return person->getDecades();}
 	void Person_delete(Person* person){
 		if (person){
