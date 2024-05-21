df33f6cf2f833c903969e6fd666c5fdbb2fcaff2
William Natvig <wina3442@arrhenius.it.uu.se>
2024-05-21 19:19:16 +0200

    Fixed indentatFixed indentation.ion.

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -14,50 +14,44 @@ def main():
 	print(f.getAge())
 	print(f.getDecades())
 
-def main():
-  
-    
-    
-    p = Person(10)
-    print("C++ Fibonacci:", p.fib())
+	p = Person(10)
+	print("C++ Fibonacci:", p.fib())
+
+	start = time.time()
+	print("Python Fibonacci:", fib_py(10))
+	print("Python Time:", time.time() - start)
+
+	start = time.time()
+	print("Numba Fibonacci:", fib_numba(10))
+	print("Numba Time:", time.time() - start)
+
+	x = range(20,30)
+	y_py = []
+	y_numba = []
+	y_cpp = []
+
+	for i in range(20, 30):
+		start = time.time()
+		Person(i)
+		p.fib()
+		y_cpp.append(time.time() - start)
+
+	for i in range(20,30):
+		start = time.time()
+		fib_py(i)
+		y_py.append(time.time() - start)
+
+	for i in range (20,30):
+		start=time.time()
+		fib_numba(i)
+		y_numba.append(time.time() - start)
+
+	plt.plot(x, y_py)
+	plt.plot(x, y_numba)
+	plt.savefig("MA4.png")
+	plt.show()
+	print (y_py, y_numba)
 
 
-    start = time.time()
-    print("Python Fibonacci:", fib_py(10))
-    print("Python Time:", time.time() - start)
-
- 
-    start = time.time()
-    print("Numba Fibonacci:", fib_numba(10))
-    print("Numba Time:", time.time() - start)
-    
-    x = range(20,30)
-    y_py = []
-    y_numba = []
-    y_cpp = []
-    
-    for i in range(20, 30):
-        start = time.time()
-        Person(i)
-        p.fib()
-        y_cpp.append(time.time() - start)
-
-    for i in range(20,30):
-        start = time.time()
-        fib_py(i)
-        y_py.append(time.time() - start)
-        
-
-    for i in range (20,30):
-        start=time.time()
-        fib_numba(i)
-        y_numba.append(time.time() - start)
-
-    plt.plot(x, y_py)
-    plt.plot(x, y_numba)
-    plt.show()
-    print (y_py, y_numba)
-    
-    
 if __name__ == '__main__':
 	main()
